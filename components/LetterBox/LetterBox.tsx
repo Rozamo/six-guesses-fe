fd382e02fa5e67bfb64711fc031bc51db7e82b2d
Rozamo <35562703+Rozamo@users.noreply.github.com>
2022-05-01 18:44:23 +0530

    feat: added initial files

---
+++ components/LetterBox/LetterBox.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const TestBox: NextPage = () => {
+  return <div>TestBox</div>;
+};
+
+const LetterBox: NextPage = () => {
+  return <div>LetterBox</div>
+};
+
+export { TestBox };
+export default LetterBox;
---
+++ components/LetterBox/index.ts
@@ -0,0 +1,2 @@
+export * from './LetterBox';
+export { default, default as LetterBox } from './LetterBox';

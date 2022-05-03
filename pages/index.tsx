7890d0d8ea14e8501338c0b60fc96c8ddc0a4a8a
Rozamo <35562703+Rozamo@users.noreply.github.com>
2022-05-03 17:39:18 +0530

    fix: removed TestBox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { LetterBox, TestBox } from "../components";
+import { LetterBox } from "../components";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -54,7 +54,6 @@ const Home: NextPage = () => {
         </div>
       </main>
       <LetterBox />
-      <TestBox />
 
       <footer className={styles.footer}>
         <a

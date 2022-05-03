190d5470f764483483b0c7225c23ab2fd3273355
Rozamo <35562703+Rozamo@users.noreply.github.com>
2022-05-03 20:38:00 +0530

    feat: added styled components and ssr config

---
+++ pages/letter-box/index.ts
@@ -0,0 +1,1 @@
+export { LetterBox as default } from "../../components";
---
+++ styles/globalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: none;
+    box-sizing: border-box;
+    background: white;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export { GlobalStyle };
